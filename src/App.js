f6a1fa5519165ec38c46194a73813e60fee69d43
vishal desai <vishaldesai030@gmail.com>
2023-07-19 18:20:26 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 import './App.css';
 
-function App() {
+const StudentRegistrationForm = () => {
+  const [name, setName] = useState('');
+  const [gender, setGender] = useState('');
+  const [graduationYear, setGraduationYear] = useState('');
+  const [dateOfBirth, setDateOfBirth] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log('Name:', name);
+    console.log('Gender:', gender);
+    console.log('Date of Birth:', dateOfBirth);
+    console.log('Graduation Year:', graduationYear);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="student-registration-form"> 
+      <h2>Student Registration Form</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="gender">Gender:</label>
+          <select
+            id="gender"
+            value={gender}
+            onChange={(e) => setGender(e.target.value)}
+          >
+            <option value="">Select Gender</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="other">Other</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="dateOfBirth">Date of Birth:</label>
+          <DatePicker
+            id="dateOfBirth"
+            selected={dateOfBirth}
+            onChange={(date) => setDateOfBirth(date)}
+            dateFormat="dd/MM/yyyy"
+            showYearDropdown
+            scrollableYearDropdown
+            yearDropdownItemNumber={15}
+            placeholderText="Select Date of Birth"
+          />
+        </div>
+        <div>
+          <label>Graduation Year:</label>
+          <div>
+            <label>
+              <input
+                type="radio"
+                value="2023"
+                checked={graduationYear === '2023'}
+                onChange={(e) => setGraduationYear(e.target.value)}
+              />{' '}
+              2023
+            </label>
+            <label>
+              <input
+                type="radio"
+                value="2024"
+                checked={graduationYear === '2024'}
+                onChange={(e) => setGraduationYear(e.target.value)}
+              />{' '}
+              2024
+            </label>
+            <label>
+              <input
+                type="radio"
+                value="2025"
+                checked={graduationYear === '2025'}
+                onChange={(e) => setGraduationYear(e.target.value)}
+              />{' '}
+              2025
+            </label>
+          </div>
+        </div>
+        <button type="submit">Register</button>
+      </form>
     </div>
   );
-}
+};
 
-export default App;
+export default StudentRegistrationForm;
